4c5f975b779dc11dd475a75e6694b962cdf64b7f
moshiax <97439761+moshiax@users.noreply.github.com>
2026-04-14 11:46:31 +0300

    Fix youtubamoledblack new queues bg

--- config.js
+++ config.js
@@ -343,6 +343,8 @@ const config = {
 				--yt-spec-raised-background: black !important;
 				--yt-spec-menu-background: black !important;
 				--yt-spec-text-primary-inverse: black !important;
+				--yt-sys-color-baseline--raised-background: black !important;
+				--yt-sys-color-baseline--base-background: black !important;
 			}
 
 			#voice-search-button.ytd-masthead {
